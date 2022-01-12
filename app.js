41d3fd8c79d7f4399c8a5abbb01e149012c2bd58
EdJaPe <ed.stu.ga.1214@gmail.com>
2022-01-12 12:23:05 -0800

    added ramdom button and displayed current gradient

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var css = document.querySelector("h3");
 var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random");
+
+function randomInt() {
+  return Math.floor(Math.random() * 256);
+}
+
+function setRandomGradient() {
+  body.style.background = `linear-gradient(to right, rgb(${randomInt()}, ${randomInt()}, ${randomInt()}), rgb(${randomInt()}, ${randomInt()}, ${randomInt()})`;
+  css.textContent = body.style.background + ";";
+}
 
 function setGradient() {
   body.style.background =
@@ -9,5 +19,8 @@ function setGradient() {
   css.textContent = body.style.background + ";";
 }
 
+setGradient();
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", setRandomGradient);
